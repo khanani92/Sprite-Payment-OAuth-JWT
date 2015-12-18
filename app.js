edb787127f089889a9f37da4afca82ecca267555
khanani92 <muddassir_92@hotmail.com>
2015-12-18 17:45:32 +0500

    ..

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use('/', router);
-
+app.use('/admin',express.static('web-portal'));
 
 
 
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-app.use('/admin',express.static('web-portal'));
+
 /// error handlers
 
 // development error handler
